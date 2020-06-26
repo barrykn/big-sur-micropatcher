45395299286df015cc24804b5b23a4e66337c2c0
Barry K. Nathan <barryn@pobox.com>
2020-06-26 04:21:59 -0700

    Split some code from run-installer.sh to insert-dylib.sh
    This begins a refactoring by taking the library insertion code from
    run-installer.sh and copying it to insert-dylib.sh. (The pmset code for
    disabling display and system sleep comes along for the ride.)
    
    run-installer.sh still contains the original code and does not yet call
    insert-dylib.sh, so there is not yet any potential for behavioral change.
    In the next phase, run-installer.sh will call insert-dylib.sh instead of
    containing a copy of the same code. Once that passes testing, I'll probably
    merge that from the dev-v0.0.4 branch to the dev-v0.0.3 branch, to minimize
    differences in code between 0.0.3 and 0.0.4. (And after that, I'll bump
    the version number on the 0.0.4 branch.)
    
    On the dev-v0.0.4 branch, the phase after that will be to rearrange things
    so that insert-dylib.sh is called not from run-installer.sh but from
    set-vars.sh, as part of a larger change in how the patcher works.

---
+++ payloads/insert-dylib.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BASE='/Volumes/Image Volume'
+
+if [ -e "$BASE/Hax2.app" ]
+then
+    echo 'Found Hax2.app, so using embedded Hax2Lib.dylib'
+    LIBPATH="$BASE/Hax2.app/Contents/Resources/Hax2Lib.dylib"
+elif [ -e "$BASE/Hax2Lib.dylib" ]
+then
+    echo 'Found Hax2Lib.dylib'
+    LIBPATH="$BASE/Hax2Lib.dylib"
+elif [ -e "$BASE/Hax.dylib" ]
+then
+    echo 'Found Hax.dylib'
+    LIBPATH="$BASE/Hax.dylib"
+else
+    echo 'Could not find Hax2.app, Hax2Lib.dylib, or Hax.dylib.'
+    echo 'Please copy one of them onto your USB stick.'
+    exit 1
+fi
+
+# On unsupported Macs, system sleep and display sleep often don't work
+# properly in the Recovery environment. So, disable them before
+# starting the installer.
+pmset -a displaysleep 0 sleep 0
+
+launchctl setenv DYLD_INSERT_LIBRARIES "$LIBPATH"
+
