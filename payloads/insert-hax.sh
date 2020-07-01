f39021c4c0ace2dc5cc7075360dcf1f69a4f7f8e
Barry K. Nathan <barryn@pobox.com>
2020-06-30 23:43:52 -0700

    Update insert-hax for Hax3

--- payloads/insert-hax.sh
+++ payloads/insert-hax.sh
@@ -2,10 +2,20 @@
 
 BASE='/Volumes/Image Volume'
 
+# I should clean this up at a later point in patcher development, but
+# for now (late June/early July) I'll keep it flexible.
+if [ -e "$BASE/Hax3.app" ]
+then
+    echo 'Found Hax3.app, so using embedded HaxLib.dylib'
+    LIBPATH="$BASE/Hax3.app/Contents/Resources/HaxLib.dylib"
 if [ -e "$BASE/Hax2.app" ]
 then
     echo 'Found Hax2.app, so using embedded Hax2Lib.dylib'
     LIBPATH="$BASE/Hax2.app/Contents/Resources/Hax2Lib.dylib"
+elif [ -e "$BASE/HaxLib.dylib" ]
+then
+    echo 'Found HaxLib.dylib (presumably from Hax3)'
+    LIBPATH="$BASE/HaxLib.dylib"
 elif [ -e "$BASE/Hax2Lib.dylib" ]
 then
     echo 'Found Hax2Lib.dylib'
