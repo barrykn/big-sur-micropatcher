805eeab3be56ffd82bd09fce1df831bec8629ecf
Barry K. Nathan <barryn@pobox.com>
2020-07-02 04:40:02 -0700

    Revert "patch-kexts.sh: remove kextcache -i; update comments"
    This reverts commit c60d47f76bb71cfe69510fe47aeaee5dfcc11803.
    
    I'll follow up with another commit to clear the comments up.

--- payloads/patch-kexts.sh
+++ payloads/patch-kexts.sh
@@ -154,7 +154,13 @@ fi
 
 popd
 
-# Now do the real kernel cache update stuff
+# "this is required to update the prelinkedkernel" per jackluke,
+# but I wonder if it's really necessary in light of the kmutil command
+# that follows. Anyway, no harm to keep it here for now, and I can do more
+# testing later.
+kextcache -i "$VOLUME"
+
+# "this is required to update the BootKernelExtensions.kc" per jackluke
 kmutil install --volume-root "$VOLUME" --update-all
 
 # The way you control kcditto's *destination* is by choosing which volume
