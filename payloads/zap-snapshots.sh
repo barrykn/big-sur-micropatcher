27078a9fd6b6a8fa9d7f9dc59d286202eef08a99
Barry K. Nathan <barryn@pobox.com>
2020-07-16 19:59:30 -0700

    zap-snapshots.sh: New script to delete all but most recent snapshot
    This script deletes all but the most recent snapshot on the APFS volume
    specified on the command line. This is the initial version of the script.
    I'll probably add more error checking before the v0.0.14 patcher release.

---
+++ payloads/zap-snapshots.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Delete all snapshots from the specified volume EXCEPT THE MOST RECENT.
+# (The combination of grep and awk used in this script automatically
+# excludes the most recent snapshot. I mention this because it is not
+# at all obvious that it does this, unless you try it against actual
+# `diskutil apfs listsnapshots` output.
+
+VOLUME="$1"
+
+if mount -uw "$VOLUME"
+then
+    # Remount succeeded. Do nothing in this block, and keep going.
+    true
+else
+    echo "Remount failed. Cannot proceed."
+    exit 1
+fi
+
+for XID in `diskutil apfs listSnapshots "$VOLUME"|fgrep XID|awk '{print $3}'`
+do
+    echo $XID
+    diskutil apfs deleteSnapshot "$VOLUME" -xid $XID
+done
