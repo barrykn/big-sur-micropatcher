a64036c5608f9fd83e194185177bfa63e75ace2e
Barry K. Nathan <barryn@pobox.com>
2020-08-09 02:56:53 -0700

    kc-list-kext-paths: Fix Xcode compatibility
    Previously this script didn't work with Xcode 10.1 under High Sierra.
    There are probably other versions of Xcode that didn't work either, but
    it's a moot point now because, with this commit, it should work with all
    Xcode versions.
    
    (Add a comment to document things a little better while I'm at it.)

--- misc-scripts/kc-list-kext-paths.sh
+++ misc-scripts/kc-list-kext-paths.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-strings "$1" | fgrep -A1 PrelinkBundlePath |
+# This script should work under both Linux and macOS. Either way, the
+# strings command is required. Under Linux, you'll probably find it in the
+# binutils package. Under macOS, you'll need either full Xcode or the
+# Xcode Command Line Tools.
+strings - "$1" | fgrep -A1 PrelinkBundlePath |
     sed -n -e 's@^.*<string>@@' -e 's@</string>.*@@p'
