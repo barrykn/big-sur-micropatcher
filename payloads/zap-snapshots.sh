8c47ffc4abc6a03363e40f66c2e4d35195055f0e
Barry K. Nathan <barryn@pobox.com>
2020-08-11 06:21:42 -0700

    zap-snapshots: Add support for running inside remount-sysvol subshell
    With this change, zap-snapshots can now be run inside a full Big Sur
    installation, in addition to working in the Recovery environment.

--- payloads/zap-snapshots.sh
+++ payloads/zap-snapshots.sh
@@ -5,20 +5,32 @@
 # at all obvious that it does this, unless you try it against actual
 # `diskutil apfs listsnapshots` output.
 
-VOLUME="$1"
-
-# Make sure a volume has been specified.
+# Check whether we have already been provided a volume via the VOLUME
+# environment variable set by remount-sysvol.sh.
 if [ -z "$VOLUME" ]
 then
-    echo 'You must specify a target volume (such as /Volumes/Macintosh\ HD)'
-    echo 'on the command line.'
-    exit 1
-fi
+    VOLUME="$1"
 
-if ! mount -uw "$VOLUME"
-then
-    echo "Remount failed. Cannot proceed."
-    exit 1
+    # Make sure a volume has been specified.
+    if [ -z "$VOLUME" ]
+    then
+        echo 'You must specify a target volume (such as /Volumes/Macintosh\ HD)'
+        echo 'on the command line, or you must first use remount-sysvol.sh,'
+        echo 'then run zap-snapshots.sh from inside the remount-sysvol.sh'
+        echo 'subshell.'
+        exit 1
+    fi
+
+    # This check certainly needs to be skipped if we're inside the
+    # remount-sysvol subshell (hence why it's inside another if-then block).
+    # However, there may be other situations where this check also needs
+    # to be skipped. Just figured I'd mention it here in case it trips
+    # anyone up later.
+    if ! mount -uw "$VOLUME"
+    then
+        echo "Remount failed. Cannot proceed."
+        exit 1
+    fi
 fi
 
 for XID in `diskutil apfs listSnapshots "$VOLUME"|fgrep XID|awk '{print $3}'`
