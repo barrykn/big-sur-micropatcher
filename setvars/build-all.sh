dcdc7a706ec33636932a1eb2a605894254325779
Barry K. Nathan <barryn@pobox.com>
2020-09-17 17:58:31 -0700

    Build both regular and verbose boot variants of setvars

--- setvars/build-all.sh
+++ setvars/build-all.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
-rm -f *.efi
-make clean
-make
-make clean
-setarch i386 make
-make clean
-make fat
 
-rm -rf EFI
+make clobber
+make
+setarch i386 make
+
+for x in setvars setvars-verboseboot
+do
+    grub-glue-efi -3 $x-ia32.efi -6 $x-x86_64.efi -o $x.efi
+done
+
 mkdir -p EFI/boot
+cp *.efi EFI/boot
+
+cp -r EFI EFI-verboseboot
 cp setvars-x86_64.efi EFI/boot/bootx64.efi
 cp setvars-ia32.efi EFI/boot/bootia32.efi
-cp *.efi EFI/boot/
+cp setvars.efi EFI/boot/boot.efi
+cp setvars-verboseboot-x86_64.efi EFI-verboseboot/boot/bootx64.efi
+cp setvars-verboseboot-ia32.efi EFI-verboseboot/boot/bootia32.efi
+cp setvars-verboseboot.efi EFI-verboseboot/boot/boot.efi
---
+++ setvars/setvars-verboseboot.c
@@ -0,0 +1,21 @@
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+    const char bootArgs[] = "-v -no_compat_check";
+    const char csrVal[4] = {'w', 0x08, 0x00, 0x00};
+
+    const EFI_GUID appleGUID = {0x7c436110, 0xab2a, 0x4bbb, {0xa8, 0x80, 0xfe, 0x41, 0x99, 0x5c, 0x9f, 0x82}};
+    const UINT32 flags = EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS|EFI_VARIABLE_NON_VOLATILE;
+   
+    EFI_RUNTIME_SERVICES* rt; 
+
+    InitializeLib(ImageHandle, SystemTable);
+    rt = SystemTable->RuntimeServices;
+
+    uefi_call_wrapper(rt->SetVariable, 5, L"csr-active-config", &appleGUID, flags, 4, csrVal);
+    uefi_call_wrapper(rt->SetVariable, 5, L"boot-args", &appleGUID, flags, sizeof(bootArgs), bootArgs);
+    uefi_call_wrapper(rt->ResetSystem, 4, EfiResetShutdown, EFI_SUCCESS, 0, NULL);
+
+    return EFI_SUCCESS;
+}
