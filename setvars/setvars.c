a5c4422639345f0bce89e2976bc0c312af00d253
Barry K. Nathan <barryn@pobox.com>
2020-09-17 21:24:58 -0700

    setvars: Enable TRIM while we're at it
    Big Sur now uses an nvram variable to keep track of the trimforce
    setting, so it's easy to enable that at the same time as the other
    changes.
    
    (I also made csrVal[] static, just in case it might make any kind of
    binary patching easier in the future.)

--- setvars/setvars.c
+++ setvars/setvars.c
@@ -7,7 +7,8 @@ EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTabl
      * of some kind later. It certainly does no harm that I can see.
      */
     static const char bootArgs[] = "-no_compat_check";
-    const char csrVal[4] = {'w', 0x08, 0x00, 0x00};
+    static const char csrVal[4] = {'w', 0x08, 0x00, 0x00};
+    static const char trimSetting[1] = {0x01};
 
     const EFI_GUID appleGUID = {0x7c436110, 0xab2a, 0x4bbb, {0xa8, 0x80, 0xfe, 0x41, 0x99, 0x5c, 0x9f, 0x82}};
     const UINT32 flags = EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS|EFI_VARIABLE_NON_VOLATILE;
@@ -19,6 +20,8 @@ EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTabl
 
     uefi_call_wrapper(rt->SetVariable, 5, L"csr-active-config", &appleGUID, flags, 4, csrVal);
     uefi_call_wrapper(rt->SetVariable, 5, L"boot-args", &appleGUID, flags, sizeof(bootArgs), bootArgs);
+    uefi_call_wrapper(rt->SetVariable, 5, L"EnableTRIM", &appleGUID, flags, 1, trimSetting);
+
     uefi_call_wrapper(rt->ResetSystem, 4, EfiResetShutdown, EFI_SUCCESS, 0, NULL);
 
     return EFI_SUCCESS;
