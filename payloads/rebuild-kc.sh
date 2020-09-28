5642bad73abcac155c9e7062fcb205348654b42c
Barry K. Nathan <barryn@pobox.com>
2020-09-28 07:09:46 -0700

    rebuild-kc.sh: Remove --bootefi option from bless
    Oops, should've done this at the same time as with (un)patch-kexts.sh
    but I missed it.
    
    (While I'm at it, I'm also wondering if this actually belongs in
    remount-sysvol.sh instead, but I'll deal with that potential issue
    later.)

--- payloads/rebuild-kc.sh
+++ payloads/rebuild-kc.sh
@@ -78,7 +78,7 @@ kmutilErrorCheck
 if [ "$VOLUME" != "/" ]
 then
     echo 'Creating new root snapshot.'
-    bless --folder "$VOLUME"/System/Library/CoreServices --bootefi --create-snapshot
+    bless --folder "$VOLUME"/System/Library/CoreServices --create-snapshot
 else
     echo 'Booted directly from volume, so skipping snapshot creation.'
 fi
