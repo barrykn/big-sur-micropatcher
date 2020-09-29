ff77f7870371468ebf6dfefd223e9d08bbb92e76
Barry K. Nathan <barryn@pobox.com>
2020-09-29 16:05:34 -0700

    bring run-installer.sh back from the dead
    ASentientBot has a method of doing clean installs of (at least some)
    Big Sur betas which don't have full installers:
    
    https://forums.macrumors.com/threads/macos-11-big-sur-on-unsupported-macs-thread.2242172/page-173?post=28930995#post-28930995
    
    There *may* be a way to use this script to make this method easier.
    I'm not sure yet. But making this script work again is the first step
    in finding out.
    
    (I have not tested it yet, so these changes may not be sufficient,
    but they're basically necessary.)

--- payloads/run-installer.sh
+++ payloads/run-installer.sh
@@ -2,29 +2,36 @@
 
 # Any command line arguments are passed to InstallAssistant.
 
-# As of micropatcher v0.0.4, this script is left present as a potential
-# convenience but is otherwise deprecated. Instead of running this script,
-# users should quit Terminal and run the Installer as normal.
+# Users should generally quit Terminal and start the Installer normally,
+# but this script may be useful under certain circumstances.
 
 
 BASE='/Volumes/Image Volume'
-APPNAME='Install macOS Beta.app'
 
-# Regarding the choice of InstallAssistant binary, there is also one
-# at /Install macOS Beta/Contents/MacOS/InstallAssistant -- but if you
+# Figure out what the installer app is called (similar to how micropatcher.sh
+# figures out the installer USB's name).
+for x in "Install macOS Big Sur" "Install macOS Big Sur Beta" "Install macOS Beta"
+do
+    if [ -d "/Volumes/$BASE/$x.app" ]
+    then
+        APPNAME="$x.app"
+        break
+    fi
+done
+
+# Micropatcher v0.0.4 introduced a requirement for insert-hax.sh to be
+# run before this script. I believe the installer trampoline introduced
+# in v0.2.0 should now handle that automatically.
+#
+# I just realized, maybe it won't work out that way. I'll actually give
+# this code a try, then I'll see what's happening.
+
+# Regarding the choice of InstallAssistant binary, there is also one at
+# /Volumes/(name of USB stick)/Contents/MacOS/InstallAssistant -- but if you
 # use that one, it fails with:
 # "Installation requires downloading important content. That content can't
 # be downloaded at this time. Try again later."
 INSTALLERNAME="$BASE/$APPNAME/Contents/MacOS/InstallAssistant"
-
-# As of micropatcher version 0.0.4, it is the user's responsibility to
-# run insert-hax.sh before running this script. Users should generally
-# run set-vars.sh, which will itself invoke insert-hax.sh; furthermore,
-# this script is now deprecated. So, this should not be a problem.
-#
-# In the event that I turn out to be mistaken, I'll reintroduce error
-# checking in this script in a future micropatcher release. (I think it's
-# more likely that this script will be removed entirely, however.)
 
 # The installer **MUST** be backgrounded using &, or else the "Close Other
 # Applications" button in the installer fails to close Terminal and the
