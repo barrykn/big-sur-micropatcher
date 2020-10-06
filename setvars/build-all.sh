85331e6683c2789f78817d7fde0a42fe11e6f2f6
Barry K. Nathan <barryn@pobox.com>
2020-10-06 04:14:54 -0700

    setvars: Add SIP/ARV-enabled variant (source)
    I did this to see if it might be possible to install and run Big Sur
    without disabling SIP or ARV, for machines which don't need kext patches.
    (The answer turned out to be yes!)

--- setvars/build-all.sh
+++ setvars/build-all.sh
@@ -24,7 +24,7 @@ make clobber
 make
 setarch i386 make
 
-for x in setvars setvars-verboseboot
+for x in setvars setvars-verboseboot setvars-enablesiparv setvars-enablesiparv-vb
 do
     grub-glue-efi -3 $x-ia32.efi -6 $x-x86_64.efi -o $x.efi
 done
@@ -33,9 +33,18 @@ mkdir -p EFI/boot
 cp *.efi EFI/boot
 
 cp -r EFI EFI-verboseboot
+cp -r EFI EFI-enablesiparv
+cp -r EFI EFI-enablesiparv-vb
+
 mv setvars-x86_64.efi EFI/boot/bootx64.efi
 mv setvars-ia32.efi EFI/boot/bootia32.efi
 mv setvars.efi EFI/boot/boot.efi
 mv setvars-verboseboot-x86_64.efi EFI-verboseboot/boot/bootx64.efi
 mv setvars-verboseboot-ia32.efi EFI-verboseboot/boot/bootia32.efi
 mv setvars-verboseboot.efi EFI-verboseboot/boot/boot.efi
+mv setvars-enablesiparv-x86_64.efi EFI-enablesiparv/boot/bootx64.efi
+mv setvars-enablesiparv-ia32.efi EFI-enablesiparv/boot/bootia32.efi
+mv setvars-enablesiparv.efi EFI-enablesiparv/boot/boot.efi
+mv setvars-enablesiparv-vb-x86_64.efi EFI-enablesiparv-vb/boot/bootx64.efi
+mv setvars-enablesiparv-vb-ia32.efi EFI-enablesiparv-vb/boot/bootia32.efi
+mv setvars-enablesiparv-vb.efi EFI-enablesiparv-vb/boot/boot.efi
---
+++ setvars/setvars-enablesiparv-vb.c
@@ -0,0 +1,36 @@
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+    /* Making bootArgs[] static keeps it contiguous within the compiled binary.
+     * This might be good in case there's a need later to do binary patching
+     * of some kind later. It certainly does no harm that I can see.
+     */
+    static const char bootArgs[] = "-v -no_compat_check";
+
+    /* 1-char array rather than just a char variable, so that I can
+     * treat it the same way as the others when calling
+     * rt->SetVariable().
+     *
+     * 0x01 enables TRIM even on non-Apple SSDs, like `trimforce enable`.
+     */
+    static const char trimSetting[1] = {0x01};
+
+    const EFI_GUID appleGUID = {0x7c436110, 0xab2a, 0x4bbb, {0xa8, 0x80, 0xfe, 0x41, 0x99, 0x5c, 0x9f, 0x82}};
+    const UINT32 flags = EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS|EFI_VARIABLE_NON_VOLATILE;
+   
+    EFI_RUNTIME_SERVICES* rt; 
+
+    InitializeLib(ImageHandle, SystemTable);
+    rt = SystemTable->RuntimeServices;
+
+    /* this deletes csr-active-config, to enable SIP and ARV */
+    uefi_call_wrapper(rt->SetVariable, 5, L"csr-active-config", &appleGUID, flags, 0, NULL);
+
+    uefi_call_wrapper(rt->SetVariable, 5, L"boot-args", &appleGUID, flags, sizeof(bootArgs), bootArgs);
+    uefi_call_wrapper(rt->SetVariable, 5, L"EnableTRIM", &appleGUID, flags, 1, trimSetting);
+
+    uefi_call_wrapper(rt->ResetSystem, 4, EfiResetShutdown, EFI_SUCCESS, 0, NULL);
+
+    return EFI_SUCCESS;
+}
